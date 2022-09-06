9154db75364b46d7a05616f64383766792347b67
muralidhar6 <98840072+muralidhar6@users.noreply.github.com>
2022-09-06 23:15:05 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,5 @@
+
+          /* var name=window.prompt("Please Enter Your Name");
+            document.write(" Hello "+name+" ! Have nice day!!");
+            name.style.color="#f5f7f7"; */
+            
